b3c446b8f3e6166578da3453a81a851c94c1765c
Chaevivin <chaevivin@gmail.com>
2022-08-10 19:44:08 +0900

    Feat : 필터 기능 추가

--- beer_community/src/main/frontend/src/components/filterBox/filterBox.jsx
+++ beer_community/src/main/frontend/src/components/filterBox/filterBox.jsx
@@ -1,13 +1,62 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styles from './filterBox.module.css';
+import { useState } from 'react';
 
-const FilterBox = (props) => {
+const FilterBox = () => {
+    const [changeColor, setChangeColor] = useState(true);
+    const [filterState, setFilterState] = useState({
+        passingTags: {
+            Lager: false,
+            Ale: false,
+            IPA: false,
+            Stout: false
+        }
+    });
+
+    const handleFilter = (e) => {
+        const beerType = e.target.id;
+        setFilterState({
+            passingTags: {...filterState.passingTags, [beerType]: !filterState.passingTags[beerType]}  // 클릭하면 true, false로 바꿔주기
+        });
+        // console.log(filterState.passingTags[beerType]);
+        if (filterState.passingTags[beerType] === false) {
+            let cur = document.getElementById(beerType);
+            cur.style.color = "white";
+            cur.style.backgroundColor = "#FDC74B";
+        } 
+        
+        if (filterState.passingTags[beerType] === true) {
+            let cur = document.getElementById(beerType);
+            cur.style.color = "#FDC74B";
+            cur.style.backgroundColor = "white";
+        }
+
+        setChangeColor(changeColor);
+    };
+
+    const filteredCollected = useCallback(() => {
+        const clicked = [];
+        const now = filterState.passingTags;
+
+        for (let item in now) {
+            if(now[item]) {
+                // clicked.now = item; -> 현재 클릭된 아이템
+                clicked.push(item);
+            }
+        }
+
+        return clicked;
+    }, [filterState.passingTags]);
+
+    const result = filteredCollected();
+    console.log(result);
+
     return (
         <div className={styles.container}>
-            <button className={styles.box}>라거</button>
-            <button className={styles.box}>에일</button>
-            <button className={styles.box}>IPA</button>
-            <button className={styles.box}>스타우트</button>
+            <button id="Lager" className={styles.box} onClick={handleFilter}>라거</button>
+            <button id="Ale" className={styles.box} onClick={handleFilter}>에일</button>
+            <button id="IPA" className={styles.box} onClick={handleFilter}>IPA</button>
+            <button id="Stout" className={styles.box} onClick={handleFilter}>스타우트</button>
         </div>
     );
 };
