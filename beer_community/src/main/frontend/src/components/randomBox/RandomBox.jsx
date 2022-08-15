31f8bad3eeacc75d2e90107180c2dbb22ae541ad
Chaevivin <chaevivin@gmail.com>
2022-08-16 01:11:34 +0900

    Fix : RandomBox.jsx에서 axios 요청

--- beer_community/src/main/frontend/src/components/randomBox/RandomBox.jsx
+++ beer_community/src/main/frontend/src/components/randomBox/RandomBox.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import styles from "./randomBox.module.css";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from "axios";
 
 const RandomBox = (props) => {
   const [beerData, setBeerData] = useState([]);
-  const [count, setCount] = useState(0);
 
   const handleButtonClick= (e) => {
     const getBeerList = async() => {
@@ -13,19 +12,24 @@ const RandomBox = (props) => {
       setBeerData(response.data.data);
     }
     getBeerList();
-    setCount(count + 1);
-    console.log(count);
   }
-
+  
+  useEffect(() => {
+    const getBeerList = async() => {
+      let response = await axios.get("/api/recommend");
+      setBeerData(response.data.data);
+    }
+    getBeerList();
+  }, []);
 
   return (
     <div className={styles.mainBox}>
       <div className={styles.title}>오늘의 맥주는?</div>
-      <img src={count > 0 ? beerData.imageUrl : props.data.imageUrl} alt="" className={styles.img} />
+      <img src={beerData.imageUrl} alt="" className={styles.img} />
       <div className={styles.detail}>
         <div className={styles.degree}>
           <p className={styles.text}>Alchol degree</p>
-          <p className={styles.degreeNum}>{count > 0 ? beerData.alcoholDegree : props.data.alcoholDegree}</p>
+          <p className={styles.degreeNum}>{beerData.alcoholDegree}</p>
         </div>
         <div className={styles.tastes}>
             <button className={styles.taste}>부드러움</button>
@@ -33,9 +37,9 @@ const RandomBox = (props) => {
         </div>
       </div>
 
-      <div className={styles.name}>{count > 0 ? beerData.beerName : props.data.beerName}</div>
+      <div className={styles.name}>{beerData.beerName}</div>
       <div className={styles.description}>
-        {count > 0 ? beerData.information : props.data.information}
+        {beerData.information}
       </div>
       <button className={styles.retry} onClick={handleButtonClick}>
         다시하기
