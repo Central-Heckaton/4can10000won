74dbc44c52d251198107447d83d03bc0d2449216
Chaevivin <chaevivin@gmail.com>
2022-08-15 19:51:01 +0900

    Chore : Random 페이지 기능 추가 및 CSS 스타일링

--- beer_community/src/main/frontend/src/components/randomBox/RandomBox.jsx
+++ beer_community/src/main/frontend/src/components/randomBox/RandomBox.jsx
@@ -1,24 +1,43 @@
 import React from "react";
-
 import styles from "./randomBox.module.css";
+import { useState } from 'react';
+import axios from "axios";
+
 const RandomBox = (props) => {
+  const [beerData, setBeerData] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const handleButtonClick= (e) => {
+    const getBeerList = async() => {
+      let response = await axios.get("/api/recommend");
+      setBeerData(response.data.data);
+    }
+    getBeerList();
+    setCount(count + 1);
+    console.log(count);
+  }
+
+
   return (
     <div className={styles.mainBox}>
       <div className={styles.title}>오늘의 맥주는?</div>
-      <img src={props.data.image_url} alt="" className={styles.img} />
+      <img src={count > 0 ? beerData.imageUrl : props.data.imageUrl} alt="" className={styles.img} />
       <div className={styles.detail}>
         <div className={styles.degree}>
           <p className={styles.text}>Alchol degree</p>
-          <p className={styles.degreeNum}>{props.data.alcohol_degree}</p>
+          <p className={styles.degreeNum}>{count > 0 ? beerData.alcoholDegree : props.data.alcoholDegree}</p>
         </div>
-        
+        <div className={styles.tastes}>
+            <button className={styles.taste}>부드러움</button>
+            <button className={styles.taste}>부드러움</button>
+        </div>
       </div>
 
-      <div className={styles.name}>{props.data.beer_name}</div>
+      <div className={styles.name}>{count > 0 ? beerData.beerName : props.data.beerName}</div>
       <div className={styles.description}>
-        {props.data.imformation}
+        {count > 0 ? beerData.information : props.data.information}
       </div>
-      <button className={styles.button}>
+      <button className={styles.retry} onClick={handleButtonClick}>
         다시하기
       </button>
     </div>
