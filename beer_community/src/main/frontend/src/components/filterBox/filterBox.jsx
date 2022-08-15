42ca703c8661d5a759184bdc44b9167c26aac6ae
Hellol77 <dhe7700@naver.com>
2022-08-15 23:36:24 +0900

    Fix : search 페이지 정렬 버그 수정

--- beer_community/src/main/frontend/src/components/filterBox/filterBox.jsx
+++ beer_community/src/main/frontend/src/components/filterBox/filterBox.jsx
@@ -1,88 +1,101 @@
-import React, { useCallback, useEffect } from 'react';
-import styles from './filterBox.module.css';
-import { useState } from 'react';
-import axios from 'axios';
+import React, { useCallback, useEffect } from "react";
+import styles from "./filterBox.module.css";
+import { useState } from "react";
+import axios from "axios";
 import BeerList from "../beerList/beerList";
 // import BeerList from "../components/beerList/beerList";
 
 const FilterBox = () => {
-    const [beerData, setBeerData] = useState([]);
-    const [changeColor, setChangeColor] = useState(true);
-    const [filterState, setFilterState] = useState({
-        passingTags: {
-            Lager: false,
-            Ale: false,
-            IPA: false,
-            Stout: false
-        }
+  const [beerData, setBeerData] = useState([]);
+  const [changeColor, setChangeColor] = useState(true);
+  const [filterState, setFilterState] = useState({
+    passingTags: {
+      Lager: false,
+      Ale: false,
+      IPA: false,
+      Stout: false,
+    },
+  });
+  const handleFilter = (e) => {
+    const beerType = e.target.id;
+    setFilterState({
+      passingTags: {
+        ...filterState.passingTags,
+        [beerType]: !filterState.passingTags[beerType],
+      }, // 클릭하면 true, false로 바꿔주기
     });
-    const handleFilter = (e) => {
-        const beerType = e.target.id;
-        setFilterState({
-            passingTags: {...filterState.passingTags, [beerType]: !filterState.passingTags[beerType]}  // 클릭하면 true, false로 바꿔주기
-        });
-        // console.log(filterState.passingTags[beerType]);
-        if (filterState.passingTags[beerType] === false) {
-            let cur = document.getElementById(beerType);
-            cur.style.color = "white";
-            cur.style.backgroundColor = "#FDC74B";
-        } 
-        
-        if (filterState.passingTags[beerType] === true) {
-            let cur = document.getElementById(beerType);
-            cur.style.color = "#FDC74B";
-            cur.style.backgroundColor = "white";
-        }
+    // console.log(filterState.passingTags[beerType]);
+    if (filterState.passingTags[beerType] === false) {
+      let cur = document.getElementById(beerType);
+      cur.style.color = "white";
+      cur.style.backgroundColor = "#FDC74B";
+    }
 
-        setChangeColor(changeColor);
-        getBeerList();
-    };
+    if (filterState.passingTags[beerType] === true) {
+      let cur = document.getElementById(beerType);
+      cur.style.color = "#FDC74B";
+      cur.style.backgroundColor = "white";
+    }
 
-    const filteredCollected = useCallback(() => {
-        const clicked = [];
-        const now = filterState.passingTags;
+    setChangeColor(changeColor);
+    getBeerList();
+  };
 
-        for (let item in now) {
-            if(now[item]) {
-                // clicked.now = item; -> 현재 클릭된 아이템
-                clicked.push(item);
-            }
-        }
+  const filteredCollected = useCallback(() => {
+    const clicked = [];
+    const now = filterState.passingTags;
 
-        return clicked;
-    }, [filterState.passingTags]);
+    for (let item in now) {
+      if (now[item]) {
+        // clicked.now = item; -> 현재 클릭된 아이템
+        clicked.push(item);
+      }
+    }
 
-    const result = filteredCollected();
-    console.log(result);
-    const getBeerList = async (e) => {
-        console.log("GetBeerList Called")
-        const request_data = {'beerTypeList': result}; // type of -> Object
-        let response = await axios({
-                method: 'post',
-                url: '/api/filter',
-                headers: {'Content-Type': 'application/json'},
-                data: JSON.stringify(request_data)
-            });
-        console.log('response: ', response);
-        setBeerData(response.data.data)
-        // response.data
-        // [{ }, { }, { } ...] -> props로 전달필요
-    };
+    return clicked;
+  }, [filterState.passingTags]);
 
-    useEffect (() => {
-        console.log("Enter UseEffect")
-        getBeerList();
-    }, []);
+  const result = filteredCollected();
+  console.log(result);
+  const getBeerList = async (e) => {
+    console.log("GetBeerList Called");
+    const request_data = { beerTypeList: result }; // type of -> Object
+    let response = await axios({
+      method: "post",
+      url: "/api/filter",
+      headers: { "Content-Type": "application/json" },
+      data: JSON.stringify(request_data),
+    });
+    console.log("response: ", response);
+    setBeerData(response.data.data);
+    // response.data
+    // [{ }, { }, { } ...] -> props로 전달필요
+  };
 
-    return (
-        <div className={styles.container}>
-            <button id="Lager" className={styles.box} onClick={handleFilter}>라거</button>
-            <button id="Ale" className={styles.box} onClick={handleFilter}>에일</button>
-            <button id="IPA" className={styles.box} onClick={handleFilter}>IPA</button>
-            <button id="Stout" className={styles.box} onClick={handleFilter}>스타우트</button>
-            <BeerList data={beerData}/>
-        </div>
-    );
+  useEffect(() => {
+    console.log("Enter UseEffect");
+    getBeerList();
+  }, []);
+
+  return (
+    <>
+      <div className={styles.container}>
+        <button id="Lager" className={styles.box} onClick={handleFilter}>
+          라거
+        </button>
+        <button id="Ale" className={styles.box} onClick={handleFilter}>
+          에일
+        </button>
+        <button id="IPA" className={styles.box} onClick={handleFilter}>
+          IPA
+        </button>
+        <button id="Stout" className={styles.box} onClick={handleFilter}>
+          스타우트
+        </button>
+      </div>
+      <BeerList data={beerData} />
+    </>
+  );
 };
 
 export default FilterBox;
