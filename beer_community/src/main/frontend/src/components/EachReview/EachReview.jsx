04cb94253a84d610f16363ecbb539a6f0ec13031
Chaevivin <chaevivin@gmail.com>
2022-08-17 03:46:18 +0900

    Feat : Review 페이지 데이터 요청 및 받아오기

--- beer_community/src/main/frontend/src/components/EachReview/EachReview.jsx
+++ beer_community/src/main/frontend/src/components/EachReview/EachReview.jsx
@@ -1,11 +1,40 @@
+// EachReview.jsx
 import React from "react";
 import { useState } from "react";
 import Comment from "../Comment/Comment";
 import ViewStar from "../ViewStar/ViewStar";
 import styles from "./eachReview.module.css";
+import axios  from 'axios';
 
 const EachReview = (props) => {
   const [showComment, setShowComment] = useState(false);
+  const [recomments, setReComments] = useState([]);
+
+  const [beerId, setBeerId] = useState(props.id);
+  const [parentId, setParentId] = useState(0);
+  const [content, setContent] = useState("");
+
+  const handleDropdownClick = (e) => {
+    setShowComment(!showComment);
+    const getRecomments = async() => {
+      let response = await axios.get(`/api/recomments/${props.id}`);
+      setReComments(response.data.data);
+    }
+    getRecomments();
+    console.log('wefjoiw: ', recomments);
+  };
+
+  const handleReClick = async(e) => {
+    const request_data = { beerId: beerId, parentId: parentId, content: content };
+    let response = await axios({
+      method: "post",
+      url: "/api/comments/write-recomment",
+      headers: { "Content-Type": "application/json" },
+      data: JSON.stringify(request_data),
+    });
+    console.log(response);
+    console.log('content', content);
+  }
 
   return (
     <>
@@ -13,16 +42,16 @@ const EachReview = (props) => {
         <div className={styles.profile}>
           <img
             className={styles.profilePhoto}
-            src="https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_730"
+            src={props.userImageUrl}
             alt=""
           />
-          <div className={styles.username}>닉네임</div>
+          <div className={styles.username}>{props.username}</div>
         </div>
         <div className={styles.reviewBox}>
-          <ViewStar />
-          <div className={styles.writeTime}>작성날짜{props.writeTime} </div>
+          <ViewStar point={props.point}/>
+          <div className={styles.writeTime}>작성날짜   {props.createdDate} </div>
           <div className={styles.review}>
-            안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요
+            {props.content}
           </div>
         </div>
         <div className={styles.dropdownBox}>
@@ -30,9 +59,7 @@ const EachReview = (props) => {
             src="/img/dropbutton.png"
             alt="dropdown"
             className={showComment ? styles.dropdownSpin : styles.dropdown}
-            onClick={() => {
-              setShowComment(!showComment);
-            }}
+            onClick={handleDropdownClick}
           ></img>
         </div>
       </div>
@@ -40,14 +67,29 @@ const EachReview = (props) => {
       {showComment ? (
         <>
           <div className={styles.dropdownMain}>
-            {/* map 함수 써야함 Comment 컴포넌트 */}
-            <Comment />
-            <Comment />
+            {recomments.map((i) => (
+              <Comment 
+                key={i.id}
+                id={i.id}
+                username={i.username}
+                content={i.content}
+                createdDate={i.createdDate}
+              />
+            ))}
 
             <div className={styles.inputBox}>
-            {/* 댓글입력 form 필요? */}
-              <input placeholder="댓글을 입력하세요." className={styles.input} type="text" />
-              <button className={styles.button} type="submit">
+              <input 
+                placeholder="댓글을 입력하세요." 
+                className={styles.input} 
+                type="text" 
+                value={content} 
+                onChange={(e) => setContent(e.target.value)} 
+              />
+              <button 
+                className={styles.button} 
+                type="submit"
+                onClick={handleReClick}  
+              >
                 <img src="/img/vector.png" alt="button" />
               </button>
             </div>
