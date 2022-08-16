6a93fc50b52d948ee27a87a19f7b9dab5d868df9
Hellol77 <dhe7700@naver.com>
2022-08-16 19:14:46 +0900

    Feat : input에 아무값도 없을때 전체 맥주 불러오기

--- beer_community/src/main/frontend/src/pages/Search.jsx
+++ beer_community/src/main/frontend/src/pages/Search.jsx
@@ -1,25 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Footer from "../components/Footer/Footer";
 import FilterBox from "./../components/filterBox/filterBox";
 import Nav from "../components/Nav/Nav";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
 
 
 const Search = (props) => {
-    const [beerData, setBeerData] = useState([]);
-    let navigate = useNavigate();
-  useEffect (() => {
-    const getBeerList = async () => {
-          console.log('Search.jsx');
-           let response = await axios.get('/api/search');
-           console.log('response.data.data: ', response.data.data); // [{}, {}, {}]
-           console.log('response.data.data[0]: ', response.data.data[0]);
-    //                {id: 1, imageUrl: 'https://res.cloudinary.com/ratebeer/image/upload/d_beer_img_default.png,f_auto/beer_271470', beerName: '클라우드 오리지널 그래비티', totalPoint: 2.48, information: '100% 몰트 리얼 맥주. 오리지널 그래비티. 풍부하고 부드러운.\n클라우드는 유럽산 홉과 독일산 효모를 사용한 프리미엄 맥주입니다.'}
-           setBeerData(response.data.data);
-        }
-       // getBeerList(); // 함수호출
-    }, []);
+  let navigate = useNavigate();
   return (
     <>
       <Nav navigate={navigate} />
