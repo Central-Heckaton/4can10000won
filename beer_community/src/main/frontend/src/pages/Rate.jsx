14a0faf35b2e9ba9ffec48495cd2516921ac4d64
Chaevivin <chaevivin@gmail.com>
2022-08-17 03:49:57 +0900

    Feat : Rate 페이지에서 데이터 보내기

--- beer_community/src/main/frontend/src/pages/Rate.jsx
+++ beer_community/src/main/frontend/src/pages/Rate.jsx
@@ -1,17 +1,20 @@
 // Rate.jsx
 import React from 'react';
 import RateStars from '../components/rateStars/rateStars';
+import Nav from "../components/Nav/Nav";
 import { useLocation } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const Rate = (props) => {
     const location = useLocation();
     const id = location.state.id;
-    console.log(location);
+    console.log(id);
+    let navigate = useNavigate();
 
     return (
         <div>
-            <RateStars
-                id={id}/>
+            <Nav navigate={navigate} />
+            <RateStars id={id}/>
         </div>
     );
 };
