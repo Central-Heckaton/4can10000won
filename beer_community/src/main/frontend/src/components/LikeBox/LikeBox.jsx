138717c9f24118da1a825c7e5cdf9a113334b93c
Hellol77 <dhe7700@naver.com>
2022-08-18 19:31:36 +0900

    Feat : like 페이지 loading 추가

--- beer_community/src/main/frontend/src/components/LikeBox/LikeBox.jsx
+++ beer_community/src/main/frontend/src/components/LikeBox/LikeBox.jsx
@@ -1,25 +1,34 @@
 import React from "react";
 import styles from "./likeBox.module.css";
 import BeerBox from "../beerBox/beerBox";
+import RandomLoading from "../randomLoading/RandomLoading";
 
 const LikeBox = (props) => {
   console.log(props.data);
   return (
     <div className={styles.mainBox}>
       <div className={styles.button}>찜한 맥주 리스트</div>
-      {Array.isArray(props.data) && props.data.length !== 0 ? (
-        props.data.map((i) => (
-          <BeerBox
-            key={i.id}
-            id={i.id}
-            image={i.imageUrl}
-            title={i.beerName}
-            description={i.information}
-            total_point={i.totalPoint}
-          />
-        ))
+      {props.loading ? (
+        <>
+          {Array.isArray(props.data) && props.data.length !== 0 ? (
+            props.data.map((i) => (
+              <BeerBox
+                key={i.id}
+                id={i.id}
+                image={i.imageUrl}
+                title={i.beerName}
+                description={i.information}
+                total_point={i.totalPoint}
+              />
+            ))
+          ) : (
+            <div className={styles.sub}>찜한 맥주가 없어요</div>
+          )}
+        </>
       ) : (
-        <div className={styles.sub}>찜한 맥주가 없어요</div>
+        <>
+          <RandomLoading />
+        </>
       )}
     </div>
   );
