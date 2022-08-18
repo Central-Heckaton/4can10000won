436e9869419e35d6638159c3f34391ed558a4dac
Chaevivin <chaevivin@gmail.com>
2022-08-18 17:50:13 +0900

    Chore : Detail, Menu 페이지에 Footer 추가

--- beer_community/src/main/frontend/src/pages/Menu.jsx
+++ beer_community/src/main/frontend/src/pages/Menu.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import MainButtons from '../components/button/main_buttons';
+import { useNavigate } from 'react-router-dom';
+import Nav from "../components/Nav/Nav";
+import Footer from '../components/Footer/Footer';
 
 const Menu = () => {
+    let navigate = useNavigate();
+
     return (
         <>
+            <Nav navigate={navigate} />
             <MainButtons />
+            <Footer />
         </>
     );    
 };
