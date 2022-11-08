578c213b89c64c97696b1383a005f5905e78228d
kyunghwan1207 <kyunghwanko1207@gmail.com>
2022-11-08 23:52:22 +0900

    Chore: ãFãë신고된 ë댓글 전체목록 조회 FE와 API ì테스íììAPI ì연동완료

--- beer_community/src/main/java/team_project/beer_community/dto/CreateReportedCommentDto.java
+++ beer_community/src/main/java/team_project/beer_community/dto/CreateReportedCommentDto.java
@@ -1,8 +1,10 @@
 package team_project.beer_community.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class CreateReportedCommentDto {
     private Long commentId;
     private String reportType;
